515efd94f8a4ec1784e3cd0c3aec6b860fdca50e
Miranda Jaroneski <miranda.jaroneski@gmail.com>
2020-01-28 14:45:30 -0500

    Updates

---
+++ precache-manifest.5078f5dd5172d9b7b657edb59bd8724c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63c1e3af19f13ca8925afaaae33a3ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db7e7240b790fc54167e",
+    "url": "/static/css/main.d24dbee8.chunk.css"
+  },
+  {
+    "revision": "9e1e223242aeaeff2117",
+    "url": "/static/js/2.0e932495.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0e932495.chunk.js.LICENSE"
+  },
+  {
+    "revision": "db7e7240b790fc54167e",
+    "url": "/static/js/main.0a558185.chunk.js"
+  },
+  {
+    "revision": "71f9061e4094581dae1b",
+    "url": "/static/js/runtime-main.724ebd9e.js"
+  }
+]);
